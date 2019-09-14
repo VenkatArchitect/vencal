5df13a995bec74d0b827e3d414134158c05b83d5
Venkatasubramaniam (Venkat) Ramakrishnan <venkat.architect@gmail.com>
2019-09-14 11:19:59 +0530

    Python Implementation File - First Version

---
+++ vencal.py
@@ -0,0 +1,15 @@
+def venkats_add (param1, param2):
+    return param1 + param2
+
+def venkats_subtract (param1, param2):
+    return param1 - param2
+
+def venkats_multiply (param1, param2):
+    return param1 * param2
+
+def venkats_divide (param1, param2):
+    if (param2 == 0):
+        return ("Invalid denominator")
+    else:
+        return (param1/param2)
+
